017fa685cf3ba1a911d0df265ebb37db32daf2e8
Timothy McCarthy <ebh042@gmail.com>
2019-05-12 12:03:53 +1000

    Only provide bracket instance where the error type is Throwable

--- catsInterop/src/main/scala/au/id/tmm/bifunctorio/catsinterop/CatsTypeclassConversions.scala
+++ catsInterop/src/main/scala/au/id/tmm/bifunctorio/catsinterop/CatsTypeclassConversions.scala
@@ -19,28 +19,32 @@ private[catsinterop] object TmmToCatsTypeclassConversionsImpls {
     override def pure[A](a: A): F[E, A] = tmmBifunctorMonad.rightPure(a)
   }
 
-  class CatsMonadErrorForTmmBME[F[+_, +_], E](implicit tmmBme: BME[F]) extends CatsMonadForTmmBifunctorMonad with cats.MonadError[F[E, +?], E] {
+  class CatsMonadErrorForTmmBME[F[+_, +_], E](implicit tmmBme: BME[F]) extends cats.MonadError[F[E, +?], E] {
+    override def flatMap[A, A1](fea: F[E, A])(f: A => F[E, A1]): F[E, A1] = tmmBme.flatMap[E, E, A, A1](fea)(f)
+
+    override def tailRecM[A, A1](a: A)(f: A => F[E, Either[A, A1]]): F[E, A1] = tmmBme.tailRecM[E, A, A1](a)(f)
+
+    override def pure[A](a: A): F[E, A] = tmmBme.rightPure(a)
+
     override def raiseError[A](e: E): F[E, A] = tmmBme.leftPure(e)
 
     override def handleErrorWith[A](fea: F[E, A])(f: E => F[E, A]): F[E, A] = tmmBme.handleErrorWith(fea)(f)
   }
 
-  class CatsBracketForTmmBracket[F[+_, +_], E](implicit tmmBracket: Bracket[F]) extends CatsMonadErrorForTmmBME with cats.effect.Bracket[F[E, +?], E] {
-    override def bracketCase[A, B](acquire: F[E, A])(use: A => F[E, B])(release: (A, cats.effect.ExitCase[E]) => F[E, Unit]): F[E, B] = {
+  class CatsBracketForTmmBracket[F[+_, +_]](implicit tmmBracket: Bracket[F]) extends CatsMonadErrorForTmmBME[F, Throwable] with cats.effect.Bracket[F[Throwable, +?], Throwable] {
+    override def bracketCase[A, B](acquire: F[Throwable, A])(use: A => F[Throwable, B])(release: (A, cats.effect.ExitCase[Throwable]) => F[Throwable, Unit]): F[Throwable, B] = {
 
-      val releaseForTmmBracket: (A, ExitCase[E, B]) => F[Nothing, _] = { case (resource, exitCase) =>
-        val catsExitCase = exitCase match {
-          case ExitCase.Succeeded(a)    => cats.effect.ExitCase.Completed
-          case ExitCase.Failed(failure) => cats.effect.ExitCase.Error(failure)
+      val releaseForTmmBracket: (A, ExitCase[Throwable, B]) => F[Nothing, _] = { case (resource, exitCase) =>
+        val catsExitCase: cats.effect.ExitCase[Throwable] = exitCase match {
+          case ExitCase.Succeeded(a)                 => cats.effect.ExitCase.Completed
+          case ExitCase.Failed(Failure.Checked(e))   => cats.effect.ExitCase.Error(e)
+          case ExitCase.Failed(Failure.Unchecked(t)) => cats.effect.ExitCase.Error(t)
         }
 
-        tmmBracket.handleErrorWith[E, Unit, Nothing](release(resource, catsExitCase)) {
-          case t: Throwable => throw t
-          case e            => throw CatsBracketForTmmBracket.FailureInResourceReleaseException[E](e)
-        }
+        tmmBracket.handleErrorWith[Throwable, Unit, Nothing](release(resource, catsExitCase))(t => throw t)
       }
 
-      tmmBracket.bracketCase[A, E, B](acquire)(releaseForTmmBracket)(use)
+      tmmBracket.bracketCase[A, Throwable, B](acquire)(releaseForTmmBracket)(use)
     }
   }
 
@@ -48,29 +52,29 @@ private[catsinterop] object TmmToCatsTypeclassConversionsImpls {
     final case class FailureInResourceReleaseException[E](cause: E) extends Exception
   }
 
-  class CatsSyncForTmmSync[F[+_, +_]](implicit tmmSync: Sync[F]) extends CatsBracketForTmmBracket[F, Throwable] with cats.effect.Sync[F[Throwable, +?]] {
+  class CatsSyncForTmmSync[F[+_, +_]](implicit tmmSync: Sync[F]) extends CatsBracketForTmmBracket[F] with cats.effect.Sync[F[Throwable, +?]] {
     override def suspend[A](thunk: => F[Throwable, A]): F[Throwable, A] = tmmSync.suspend(thunk)
   }
 
   class CatsAsyncForTmmAsync[F[+_, +_]](implicit tmmAsync: Async[F]) extends CatsSyncForTmmSync[F] with cats.effect.Async[F[Throwable, +?]] {
     override def async[A](k: (Either[Throwable, A] => Unit) => Unit): F[Throwable, A] = tmmAsync.async(k)
-    override def asyncF[A](k: (Either[Throwable, A] => Unit) => F[Throwable, Unit]): F[Throwable, A] = tmmAsync.asyncF[Throwable, A](k.andThen(makeFailureUnchecked))
+    override def asyncF[A](k: (Either[Throwable, A] => Unit) => F[Throwable, Unit]): F[Throwable, A] = tmmAsync.asyncF[Throwable, A](k.andThen(makeFailureUnchecked(_)))
   }
 
   class CatsConcurrentForTmmConcurrent[F[+_, +_]](implicit tmmConcurrent: Concurrent[F]) extends CatsAsyncForTmmAsync with cats.effect.Concurrent[F[Throwable, +?]] {
     import CatsConcurrentForTmmConcurrent.asCatsFiber
 
-    override def start[A](fa: F[Throwable, A]): F[Throwable, cats.effect.Fiber[F[Throwable, +?], A]] = map(tmmConcurrent.start(fa))(asCatsFiber)
+    override def start[A](fa: F[Throwable, A]): F[Throwable, cats.effect.Fiber[F[Throwable, +?], A]] = tmmConcurrent.map(tmmConcurrent.start(fa))(asCatsFiber)
 
     override def racePair[A, B](fa: F[Throwable, A], fb: F[Throwable, B]): F[Throwable, Either[(A, cats.effect.Fiber[F[Throwable, +?], B]), (cats.effect.Fiber[F[Throwable, +?], A], B)]] =
-      map(tmmConcurrent.racePair(fa, fb)) {
+      tmmConcurrent.map(tmmConcurrent.racePair(fa, fb)) {
         case Left((a, bFiber))  => Left((a, asCatsFiber(bFiber)))
         case Right((aFiber, b)) => Right((asCatsFiber(aFiber), b))
       }
 
     override def race[A, B](fa: F[Throwable, A], fb: F[Throwable, B]): F[Throwable, Either[A, B]] = tmmConcurrent.race(fa, fb)
 
-    override def cancelable[A](k: (Either[Throwable, A] => Unit) => cats.effect.CancelToken[F[Throwable, +?]]): F[Throwable, A] = tmmConcurrent.cancelable(k.andThen(makeFailureUnchecked))
+    override def cancelable[A](k: (Either[Throwable, A] => Unit) => cats.effect.CancelToken[F[Throwable, +?]]): F[Throwable, A] = tmmConcurrent.cancelable(k.andThen(makeFailureUnchecked(_)))
   }
 
   object CatsConcurrentForTmmConcurrent {
@@ -98,7 +102,7 @@ trait TmmToCatsTypeclassConversions {
   implicit def tmmBifunctorIsCatsBifunctor[F[_, _] : Bifunctor]: cats.Bifunctor[F] = new CatsBifunctorForTmmBifunctor[F]()
   implicit def tmmBifunctorMonadIsCatsMonad[F[+_, +_] : BifunctorMonad, E]: cats.Monad[F[E, +?]] = new CatsMonadForTmmBifunctorMonad[F, E]()
   implicit def tmmBifunctorMonadErrorIsCatsMonadError[F[+_, +_] : BifunctorMonadError, E]: cats.MonadError[F[E, +?], E] = new CatsMonadErrorForTmmBME[F, E]()
-  implicit def tmmBracketIsCatsBracket[F[+_, +_] : Bracket, E]: cats.effect.Bracket[F[E, +?], E] = new CatsBracketForTmmBracket[F, E]()
+  implicit def tmmBracketIsCatsBracket[F[+_, +_] : Bracket]: cats.effect.Bracket[F[Throwable, +?], Throwable] = new CatsBracketForTmmBracket[F]()
   implicit def tmmSyncIsCatsSync[F[+_, +_] : Sync]: cats.effect.Sync[F[Throwable, +?]] = new CatsSyncForTmmSync[F]()
   implicit def tmmAsyncIsCatsAsync[F[+_, +_] : Async]: cats.effect.Async[F[Throwable, +?]] = new CatsAsyncForTmmAsync[F]()
   implicit def tmmConcurrentIsCatsConcurrent[F[+_, +_] : Concurrent]: cats.effect.Concurrent[F[Throwable, +?]] = new CatsConcurrentForTmmConcurrent[F]()
@@ -107,14 +111,16 @@ trait TmmToCatsTypeclassConversions {
 
 trait CatsToTmmTypeclassConversions {
 
-  implicit def catsBifunctorIsTmmBifunctor[F[_, _] : cats.Bifunctor]: Bifunctor[F] = new Bifunctor[F] {
-    override def biMap[L1, R1, L2, R2](f: F[L1, R1])(leftF: L1 => L2, rightF: R1 => R2): F[L2, R2] = cats.Bifunctor[F].bimap(f)(leftF, rightF)
+  implicit def catsBifunctorIsTmmBifunctor[F[_, _]](implicit catsBifunctor: cats.Bifunctor[F]): Bifunctor[F] = new Bifunctor[F] {
+    override def biMap[L1, R1, L2, R2](f: F[L1, R1])(leftF: L1 => L2, rightF: R1 => R2): F[L2, R2] = catsBifunctor.bimap(f)(leftF, rightF)
 
-    override def leftMap[L1, R, L2](f: F[L1, R])(leftF: L1 => L2): F[L2, R] = cats.Bifunctor[F].leftMap(f)(leftF)
+    override def leftMap[L1, R, L2](f: F[L1, R])(leftF: L1 => L2): F[L2, R] = catsBifunctor.leftMap(f)(leftF)
   }
 
 }
 
-trait CatsTypeclassConversions extends TmmToCatsTypeclassConversions with CatsToTmmTypeclassConversions
+object CatsToTmmTypeclassConversions extends CatsToTmmTypeclassConversions
+
+trait CatsTypeclassConversions extends TmmToCatsTypeclassConversions
 
 object CatsTypeclassConversions extends CatsTypeclassConversions
