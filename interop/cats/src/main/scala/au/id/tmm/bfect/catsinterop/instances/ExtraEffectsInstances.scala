e8287587e7ad7d8934a1e1ff3e196ddf2857b55b
Timothy McCarthy <ebh042@gmail.com>
2020-12-20 22:28:59 +1030

    Add EitherT instance for extra effects

---
+++ interop/cats/src/main/scala/au/id/tmm/bfect/catsinterop/instances/ExtraEffectsInstances.scala
@@ -0,0 +1,11 @@
+package au.id.tmm.bfect.catsinterop.instances
+
+import au.id.tmm.bfect.effects.extra._
+import cats.data.EitherT
+
+class ExtraEffectsInstances[F[_] : cats.effect.Sync] private[instances]
+  extends SyncInstance[F]
+    with Calendar.Live[EitherT[F, *, *]]
+    with Console.Live[EitherT[F, *, *]]
+    with EnvVars.Live[EitherT[F, *, *]]
+    with Resources.Live[EitherT[F, *, *]]
