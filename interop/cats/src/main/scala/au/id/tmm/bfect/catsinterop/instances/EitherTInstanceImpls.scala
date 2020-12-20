127c6ddf8495043da7ff26da4caa5f7159230ef0
Timothy McCarthy <ebh042@gmail.com>
2020-12-20 22:01:20 +1030

    Small format

--- interop/cats/src/main/scala/au/id/tmm/bfect/catsinterop/instances/EitherTInstanceImpls.scala
+++ interop/cats/src/main/scala/au/id/tmm/bfect/catsinterop/instances/EitherTInstanceImpls.scala
@@ -11,9 +11,7 @@ import cats.effect.{CancelToken, Fiber}
 import java.time.{Duration, Instant}
 import scala.concurrent.CancellationException
 
-class EitherTBfectDie[F[_]](implicit F: MonadError[F, Throwable])
-    extends BMEInstance[F]
-    with Die[EitherT[F, *, *]] {
+class EitherTBfectDie[F[_]](implicit F: MonadError[F, Throwable]) extends BMEInstance[F] with Die[EitherT[F, *, *]] {
   override def failUnchecked(t: Throwable): EitherT[F, Nothing, Nothing] =
     EitherT.liftF[F, Nothing, Nothing](F.raiseError[Nothing](t))
 }
