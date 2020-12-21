3845c70980bbd3efde0413790d70661532f1aac7
Timothy McCarthy <ebh042@gmail.com>
2020-12-21 15:29:53 +1030

    Rename instances for EitherT

--- interop/cats/src/main/scala/au/id/tmm/bfect/catsinterop/instances/EitherTInstances.scala
+++ interop/cats/src/main/scala/au/id/tmm/bfect/catsinterop/instances/EitherTInstances.scala
@@ -4,41 +4,46 @@ import au.id.tmm.bfect
 import cats.data.EitherT
 import cats.{Functor, Monad}
 
-// TODO need names for these instances that will not collide with any other imports
-
 trait EitherTInstanceTraits0 extends EitherTInstanceTraits1 {
-  implicit def extraEffectsInstances[F[_] : cats.effect.Sync]: AnyRef
+  implicit def bfectExtraEffectInstanceForCatsEitherT[F[_] : cats.effect.Sync]: AnyRef
     with bfect.effects.extra.Calendar[EitherT[F, *, *]]
     with bfect.effects.extra.Console[EitherT[F, *, *]]
     with bfect.effects.extra.EnvVars[EitherT[F, *, *]]
     with bfect.effects.extra.Resources[EitherT[F, *, *]] = new ExtraEffectsInstances[F]
 
-  implicit def timerInstance[F[_] : cats.effect.Timer : Monad]: bfect.effects.Timer[EitherT[F, *, *]] =
+  implicit def bfectTimerInstanceForCatsEitherT[
+    F[_] : cats.effect.Timer : Monad,
+  ]: bfect.effects.Timer[EitherT[F, *, *]] =
     new TimerInstance[F]
 
-  implicit def bracketInstance[F[_]](
+  implicit def bfectBracketInstanceForCatsEitherT[F[_]](
     implicit
     bracket: cats.effect.Bracket[F, Throwable],
   ): bfect.effects.Bracket[EitherT[F, *, *]] = new BracketInstance[F]
 
-  implicit def concurrentInstance[F[_] : cats.effect.Concurrent]: bfect.effects.Concurrent[EitherT[F, *, *]] =
+  implicit def bfectConcurrentInstanceForCatsEitherT[
+    F[_] : cats.effect.Concurrent,
+  ]: bfect.effects.Concurrent[EitherT[F, *, *]] =
     new ConcurrentInstance[F]
 
-  implicit def asyncInstance[F[_] : cats.effect.Async]: bfect.effects.Async[EitherT[F, *, *]] = new AsyncInstance[F]
+  implicit def bfectAsyncInstanceForCatsEitherT[F[_] : cats.effect.Async]: bfect.effects.Async[EitherT[F, *, *]] =
+    new AsyncInstance[F]
 
 }
 
 private[instances] trait EitherTInstanceTraits1 extends EitherTInstanceTraits2 {
 
-  implicit def nowInstance[F[_] : cats.effect.Clock : Functor]: bfect.effects.Now[EitherT[F, *, *]] = new NowInstance[F]
+  implicit def bfectNowInstanceForCatsEitherT[F[_] : cats.effect.Clock : Functor]: bfect.effects.Now[EitherT[F, *, *]] =
+    new NowInstance[F]
 
-  implicit def syncInstance[F[_] : cats.effect.Sync]: bfect.effects.Sync[EitherT[F, *, *]] = new SyncInstance[F]
+  implicit def bfectSyncInstanceForCatsEitherT[F[_] : cats.effect.Sync]: bfect.effects.Sync[EitherT[F, *, *]] =
+    new SyncInstance[F]
 
 }
 
 private[instances] trait EitherTInstanceTraits2 extends EitherTInstanceTraits3 {
 
-  implicit def dieInstance[F[_]](
+  implicit def bfectDieInstanceForCatsEitherT[F[_]](
     implicit
     monadError: cats.MonadError[F, Throwable],
   ): bfect.effects.Die[EitherT[F, *, *]] = new DieInstance[F]
@@ -47,12 +52,14 @@ private[instances] trait EitherTInstanceTraits2 extends EitherTInstanceTraits3 {
 
 private[instances] trait EitherTInstanceTraits3 extends EitherTInstanceTraits4 {
 
-  implicit def bmeInstance[F[_] : Monad]: bfect.BifunctorMonadError[EitherT[F, *, *]] = new BMEInstance[F]
+  implicit def bfectBmeInstanceForCatsEitherT[F[_] : Monad]: bfect.BifunctorMonadError[EitherT[F, *, *]] =
+    new BMEInstance[F]
 
 }
 
 private[instances] trait EitherTInstanceTraits4 {
 
-  implicit def bfectBifunctorInstance[F[_] : Functor]: bfect.Bifunctor[EitherT[F, *, *]] = new BifunctorInstance[F]
+  implicit def bfectBifunctorInstanceForCatsEitherT[F[_] : Functor]: bfect.Bifunctor[EitherT[F, *, *]] =
+    new BifunctorInstance[F]
 
 }
